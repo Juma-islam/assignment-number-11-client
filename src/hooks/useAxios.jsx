02afbc72154983268ad66616b6af44b89da06f97
Juma-islam <ononnaislamj@gmail.com>
2025-12-12 19:12:48 +0600

    added verified token

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,11 +1,33 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useEffect } from "react";
 
+ const fetchAxios = axios.create({
+        baseURL: 'http://localhost:5000'
+    })
 
 const useAxios = () => {
 
-    const fetchAxios = axios.create({
-        baseURL: 'http://localhost:5000'
-    })
+  const { user } = useAuth();
+
+  useEffect(() => {
+    // Request interceptor
+    const requestInterceptor = fetchAxios.interceptors.request.use(
+      (config) => {
+        if (user?.accessToken) {
+          config.headers.Authorization = `Bearer ${user.accessToken}`;
+        }
+        return config;
+      },
+      (error) => {
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      fetchAxios.interceptors.request.eject(requestInterceptor);
+    };
+  }, [user]);
 
   return fetchAxios;
 }
